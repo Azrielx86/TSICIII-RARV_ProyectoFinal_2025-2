9a6ccbce4a05cc62a8480781fdd42b2748633f9a
Azrielx86 <edgar1569@outlook.com>
2025-05-01 19:03:02 -0600

    Fixed Android touch, added HCSR04 and dummy markers

--- Assets/Scripts/DevBoard.cs
+++ Assets/Scripts/DevBoard.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// TODO : Change name to ElectonicComponent
+/// </summary>
 public class DevBoard : MonoBehaviour
 {
     public new string name;
@@ -13,9 +16,32 @@ public class DevBoard : MonoBehaviour
 
     private void OnMouseDown()
     {
+#if !PLATFORM_ANDROID || UNITY_EDITOR
         if (EventSystem.current.IsPointerOverGameObject())
             return;
-        
+
         selectorUI.ShowPins(pins);
+#endif
+    }
+
+    
+    private void Update()
+    {
+#if (PLATFORM_ANDROID || UNITY_ANDROID) && !UNITY_EDITOR
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Began) continue;
+
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+                return;
+
+            if (Camera.main is null) continue;
+
+            var ray = Camera.main.ScreenPointToRay(touch.position);
+            if (!Physics.Raycast(ray, out RaycastHit hit)) continue;
+            if (hit.transform == transform)
+                selectorUI.ShowPins(pins);
+        }
+#endif
     }
 }
